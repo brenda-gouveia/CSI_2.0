fbd6342da4a840f5fd26e85239d1097fb5d4c958
brendagouveia <brendagouveia@id.uff.br>
2023-03-27 22:59:20 -0300

    inseri a coluna tempo nos dados csi

--- CSI_bpm/dataset/coleta.py
+++ CSI_bpm/dataset/coleta.py
@@ -4,6 +4,39 @@ import decoders.interleaved as decoder
 from plotters.AmpPhaPlotter import Plotter
 import os
 import time
+from CSIKit.reader import get_reader
+from CSIKit.util import csitools
+import datetime
+import pandas as pd
+
+def tempo_segundos(pcap):
+
+    time = csikit(pcap)
+    data = []
+    for x in time:
+        hm = datetime.fromtimestamp(x)
+        obj = pd.to_datetime(hm)
+        data.append(obj)
+        
+    return data
+
+
+def csikit(pcap):
+    my_reader = get_reader(pcap)
+    csi_data = my_reader.read_file(pcap, scaled=True)
+
+    return csi_data.timestamps
+
+
+def add_time(pcap, dados_csi):
+    
+    dados_csi.insert(loc=0, column='tempo', value = tempo_segundos(pcap))
+
+    return dados_csi
+
+
+
+
 
 
 def process_pcap_file(pcap_filename, caminho):
@@ -17,9 +50,11 @@ def process_pcap_file(pcap_filename, caminho):
     except FileNotFoundError:
         print(f'File {pcap_filepath} not found.')
         exit(-1)
-    
 
     csi_data = samples.get_pd_csi()
+
+    csi_data = add_time(pcap_filepath, csi_data)
+
     return analyze(csi_data)
     
     
